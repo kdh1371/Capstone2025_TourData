78778063b0e5eaf266ef7b98b96b980510ae876c
kasen <donghyun1117@kangwon.ac.kr>
2025-09-23 23:38:25 +0900

    new projects

---
+++ back/tourdata/src/main/java/com/capstone/tourdata/TourdataApplication.java
@@ -0,0 +1,14 @@
+package com.capstone.tourdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class TourdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourdataApplication.class, args);
+	}
+
+}
---
+++ back/tourdata/src/test/java/com/capstone/tourdata/TourdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.tourdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
